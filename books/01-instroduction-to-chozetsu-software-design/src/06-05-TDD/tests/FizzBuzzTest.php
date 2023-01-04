9fa52b8dbdf5008f1c722ceccc3d0d8530e90271
pphos <coc4h8conhc6h12nh@gmail.com>
2023-01-04 11:16:33 +0900

    [feat]ちようぜつソフトウェア設計入門 6-5 TDDのサンプル読了

---
+++ books/01-instroduction-to-chozetsu-software-design/src/06-05-TDD/tests/FizzBuzzTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FizzBuzz;
+
+use FizzBuzz\Spec\CyclicNumberRule;
+use FizzBuzz\Core\NumberConverter;
+use FizzBuzz\Spec\PassThroughRule;
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase
+{
+  public function testFizzBuzz()
+  {
+    $fizzBuzz = new NumberConverter([
+      new CyclicNumberRule(3, "Fizz"),
+      new CyclicNumberRule(5, "Buzz"),
+      new PassThroughRule(),
+    ]);
+    $this->assertEquals("1", $fizzBuzz->convert(1));
+    $this->assertEquals("2", $fizzBuzz->convert(2));
+    $this->assertEquals("Fizz", $fizzBuzz->convert(3));
+    $this->assertEquals("Buzz", $fizzBuzz->convert(5));
+    $this->assertEquals("FizzBuzz", $fizzBuzz->convert(15));
+  }
+}
